5906b707d3dc1c6aed8aa6c4564c5b1178c1f8fc
Rodrigo Tejero <rodrigo.tejero@chimp.mx>
2024-11-28 09:08:31 -0600

    🔧 Refactor user-related routes and remove agent-related routes
    - Removed routes for getting agents, creating agents, getting agent details, and updating agent information.
    - Updated the package.json file with the correct name and version.
    - Removed the Agent model from the Prisma schema.
    
    This commit simplifies the codebase by removing unnecessary agent-related functionality.

--- entities/users/users.js
+++ entities/users/users.js
@@ -16,23 +16,8 @@ router.post('/google/authenticate', UserController.googleAuth);
 // get user avatar
 router.get('/:id/avatar', UserController.avatar);
 
-// get agents
-router.get('/:id/agents',auth, UserController.getAgents);
-
-// create new agent
-router.post('/:id/agents', auth, UserController.createAgent);
-
-// get agent
-router.get('/:id/agents/:idAgent', auth, UserController.getAgent);
-
-// update agent
-router.put('/:id/agents/:idAgent', auth, UserController.updateAgent);
-
 // init a chat
 router.post('/:id/chat', auth, UserController.initChat);
-
-// create agent entity
-router.post('/:id/agents/:idAgent/entities', auth, UserController.createEntity);
 
 // ---------------------------------------------------------------------------------------------------------------------
 
